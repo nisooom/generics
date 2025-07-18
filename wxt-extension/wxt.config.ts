a07ac63f83e25e3cc00c4890944459ffea9b44aa
TejasBhovad <tejasbhovad@gmail.com>
2025-07-18 12:29:45 +0530

    feat: add tailwind

--- wxt-extension/wxt.config.ts
+++ wxt-extension/wxt.config.ts
@@ -1,6 +1,10 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
+  modules: ["@wxt-dev/module-react"],
+  srcDir: "src",
+  manifest: {
+    permissions: ["activeTab", "scripting", "http://127.0.0.1:8000/*"],
+  },
 });
